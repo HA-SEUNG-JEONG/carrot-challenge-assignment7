398df1ef3c903bdf78f3bb72b4e6ae3c2f8587c8
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-07-02 09:18:35 +0900

    assignment7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function App() {
+export default function IndexPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
+
+  const [submitted, setSubmitted] = useState("");
+
+  const onValid = (data, event) => {
+    event.preventDefault();
+    console.log(data);
+    setSubmitted("thank you");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onValid)}>
+      <div>
+        <label htmlFor="name">Name </label>
+        <input
+          {...register("name", {
+            required: "Please Write down your name",
+          })}
+          type="text"
+          placeholder="name"
+        />
+        {errors.name?.message}
+      </div>
+      <div>
+        <label htmlFor="email">Email </label>
+        <input
+          {...register("email", {
+            required: "Please Write down your Email",
+            validate: {
+              onlyNaver: (value) =>
+                value.includes("@naver.com") || "only @naver.com",
+            },
+          })}
+          type="text"
+          placeholder="only@naver.com"
+        />
+        {errors.email?.message}
+      </div>
+      <div>
+        <label htmlFor="password">Password </label>
+        <input
+          {...register("password", {
+            required: "Please Write down password",
+            minLength: {
+              message: "Password has to be more than 10 words",
+              value: 10,
+            },
+          })}
+          type="password"
+          placeholder="minlength 10 characters"
+        />
+        {errors.password?.message}
+      </div>
+      <input type="submit" value="Log in" />
+      {submitted}
+    </form>
   );
 }
-
-export default App;
